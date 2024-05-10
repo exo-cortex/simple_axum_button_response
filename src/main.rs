4ccb6b0a39f40864826bb941f09791a62b71a813
lukas <lukas.roesel@newtec.de>
2024-05-10 16:39:23 +0200

    Add different camera resolutions to call to libcamera. Add json-deserialization on server side of clients Post requests. Add parameterized click_button()-function in index.html.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use axum::Json;
+use serde::Deserialize;
+
 use std::path::PathBuf;
 use tokio::net::TcpListener;
 use {
@@ -12,21 +15,6 @@ mod args;
 mod system_calls;
 use crate::args::Arguments;
 
-async fn handle_post(word: String) {
-    // Print the word received from the POST request
-    match word.as_str() {
-        "{\"button\":\"pressed\"}" => {
-            println!("attempting to use camera.");
-            let output = system_calls::update_image();
-            println!("{}", output);
-        }
-        word => println!("Received: {}", word),
-    }
-
-    // Return a response, if needed
-    // "Word received successfully".to_string()
-}
-
 #[tokio::main]
 async fn main() {
     let args: Arguments = argh::from_env();
@@ -35,7 +23,7 @@ async fn main() {
 
     // Create an Axum router with a route to handle POST requests to the "/send" endpoint
     let app = Router::new()
-        .route("/send", post(handle_post))
+        .route("/send", post(handler_camera_button))
         .fallback_service(routes_static(html_location));
 
     // Start the server
@@ -52,4 +40,28 @@ async fn main() {
 
 fn routes_static(path: PathBuf) -> Router {
     Router::new().nest_service("/", get_service(ServeDir::new(path.as_os_str())))
+}
+
+#[derive(Debug, Deserialize)]
+struct ButtonParams {
+    signal: Option<String>,
+}
+
+async fn handler_camera_button(Json(params): Json<ButtonParams>) {
+    // println!("received: {:?}", params);
+
+    match params.signal.as_deref() {
+        Some("camera_button_pressed") => {
+            println!("camera button press detected, attempting to use camera...");
+            let output = system_calls::update_image();
+            println!("done. status: {}", output);
+        }
+        Some("clicked") => {
+            println!("button clicked, not yet assigned to server function");
+            let output = system_calls::update_image();
+            println!("done {}", output);
+        }
+        Some(something) => println!("received unexpected signal: {}", something),
+        None => println!("camera_button signal received but contained nothing."),
+    }
 }
--- src/system_calls.rs
+++ src/system_calls.rs
@@ -4,32 +4,29 @@ use std::str;
 
 use chrono::Utc;
 
+const CAMERA_MODES: [(u16, u16); 4] = [(640, 480), (1296, 972), (1920, 1080), (2592, 1944)];
+
 pub fn update_image() -> String {
     let now = Utc::now();
 
+    println!("{}", now.format("%Y_%m_%d-%H%M%S taking new impage"));
     // let filename = format!("test_{}", now.time());
 
-    // let (width, height) = (640, 480);
-    // let fullargs = format!(
-    //     "--width {} --height {} -o {} ",
-    //     width, height, "~/webserver/img/example.jpg"
-    // );
-    // let resolution = format!("--width {} --height {} -o {}", width, height);
-    // let image_file = format!("-o ~/webserver/img/example.jpg");
+    let (width, height) = CAMERA_MODES[3];
 
     let output = Command::new("libcamera-still")
         .current_dir("/home/nt_user/webserver/img/")
         .args([
             "-n",    // do not show image on desktop (is running headless anyway)
-            "-t 50", // timeout value
-            "--width=480",
-            "--height=300",
+            "-t 50", // timeout value in ms
+            &format!("--width={}", width),
+            &format!("--height={}", height),
             "-oexample.jpg", // only works correctly without spaces
         ])
         .output()
         .expect("failed to execute command");
 
-    let bash_output = output.stdout;
+    let bash_output = &output.stdout;
 
     let s: String = match str::from_utf8(&bash_output) {
         Ok(v) => v.into(),
